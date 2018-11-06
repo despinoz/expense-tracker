fbabd94da46b570adf930d985d355f573234a967
Diego Espinoza <diespinozas@gmail.com>
2018-11-05 22:36:42 -0500

    added base form to input expenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value.split('\n'));
+    event.preventDefault();
+  }
+
   render() {
-    return <div>Expense Tracker</div>;
+    return (
+      <div>
+        <h1>Expense Tracker</h1>
+        <form onSubmit={this.handleSubmit}>
+          <textarea
+            rows="50"
+            cols="80"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
